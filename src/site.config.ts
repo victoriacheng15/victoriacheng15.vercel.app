ef86b1ec0f2dc5e7ec0d687dcd51cf17c45ef156
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-03-10 11:43:08 -0700

    chore: update githubUrl (#3)

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,7 +5,7 @@ const siteMeta = {
 		"As a software developer, my goal is to continuously grow and improve my skills. I am interested in staying current with the latest tech trends and making meaningful contributions to open-source projects.",
 	lang: "en-us",
 	ogLocale: "en-us",
-	githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+	githubUrl: "https://github.com/victoriacheng15/personal-blog-site",
 	themeColorLight: "#fafafa",
 	themeColorDark: "#1d1f21",
 };
