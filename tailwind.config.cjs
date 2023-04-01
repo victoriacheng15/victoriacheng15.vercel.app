9057f2978e75b74903177b4bfa869a1d7c13d2c7
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-04-01 10:25:02 -0600

    Chore/update packages (#97)
    * chore: change to npm
    
    * chore: update packages to latest
    
    * chore: remove line camp from plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -94,7 +94,6 @@ module.exports = {
 	},
 	plugins: [
 		require("@tailwindcss/typography"),
-		require("@tailwindcss/line-clamp"),
 		require("@tailwindcss/aspect-ratio"),
 		plugin(function ({ addComponents }) {
 			addComponents({
