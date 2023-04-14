ae14410926b1c7116eeb2d295aabd23d7f9baedb
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-04-14 15:02:44 -0600

    Reorganize files (#107)
    * chore: move skills to its own file
    
    * chore: update file path for SKILLS
    
    * chore: move SKILLS and PORJECTS out
    
    * chore: remove unused import and comment
    
    * style: the code has been formatted!
    
    ---------
    
    Co-authored-by: victoriacheng15 <victoriacheng15@users.noreply.github.com>

---
+++ src/data/skills.ts
@@ -0,0 +1,74 @@
+export const SKILLS = [
+	{
+		name: "TypeScript",
+		skill: "simple-icons:typescript",
+	},
+	{
+		name: "JavaScript",
+		skill: "simple-icons:javascript",
+	},
+	{
+		name: "Next.js",
+		skill: "simple-icons:nextdotjs",
+	},
+	{
+		name: "React",
+		skill: "simple-icons:react",
+	},
+	{
+		name: "React Router",
+		skill: "simple-icons:reactrouter",
+	},
+	{
+		name: "Jest",
+		skill: "simple-icons:jest",
+	},
+	{
+		name: "Node.js",
+		skill: "simple-icons:nodedotjs",
+	},
+	{
+		name: "Express",
+		skill: "simple-icons:express",
+	},
+	{
+		name: "Redux",
+		skill: "simple-icons:redux",
+	},
+	{
+		name: "Github Actions",
+		skill: "simple-icons:githubactions",
+	},
+	{
+		name: "Testing Library",
+		skill: "simple-icons:testinglibrary",
+	},
+	{
+		name: "Tailwind CSS",
+		skill: "simple-icons:tailwindcss",
+	},
+	{
+		name: "SASS",
+		skill: "simple-icons:sass",
+	},
+	{
+		name: "MongoDB",
+		skill: "simple-icons:mongodb",
+	},
+	{
+		name: "Bash",
+		skill: "simple-icons:gnubash",
+	},
+	{
+		name: "HTML 5",
+		skill: "simple-icons:html5",
+	},
+	{
+		name: "CSS 3",
+		skill: "simple-icons:css3",
+	},
+	{
+		name: "Cypress",
+		skill: "simple-icons:cypress",
+	},
+];
