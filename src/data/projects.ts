2749546e23b5957fbe4ead8125634f800d2d9e4c
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-14 15:03:56 -0600

    feat: add projects contains all a list of porject

---
+++ src/data/projects.ts
@@ -0,0 +1,63 @@
+export const PROJECTS = [
+	{
+		title: "Bubble Tea App",
+		code: "https://github.com/victoriacheng15/bubble-tea-api#readme",
+		live: "https://bubble-tea.cyclic.app/",
+		backend: "",
+		description:
+			"Developed an app where users can submit their favorite bubble tea combinations and view a leaderboard that ranks the popularity of these combinations. Whether you're a bubble tea aficionado or just looking for some inspiration for your next drink, this app is sure to satisfy your cravings.",
+		techs: ["Node.js", "Express", "JavaScript", "MongoDB", "CSS"],
+		isDevelop: false,
+	},
+	{
+		title: "Country Information App",
+		code: "https://github.com/victoriacheng15/rest-countries-api-ts#readme",
+		live: "https://rest-countries-api-ts-vc.netlify.app/",
+		backend: "",
+		description:
+			"Devloped an app that display short information about various countries on cards with load more button at the end of the page. Users can search countries based on text and region. Each card leads to a seperated page with more detialed information about the corresponding coutnry.",
+		techs: [
+			"TypeScript",
+			"Next.js",
+			"Redux ToolKit Query",
+			"Tailwind CSS",
+			"Testing Library",
+			"Jest",
+		],
+		isDevelop: false,
+	},
+	{
+		title: "Hacker News App",
+		code: "https://github.com/victoriacheng15/hacker-news-next#readme",
+		live: "https://hacker-news-next.vercel.app/",
+		backend: "",
+		description:
+			"Developed a Hacker News clone that allows users to view the most recent eight stories for each category on the home page, and then browse additional stories on subsequent pages. The clone provides an intuitive interface for users to stay updated on the latest technology news.",
+		techs: [
+			"TypeScript",
+			"Next.js",
+			"Redux Toolkit",
+			"Chakra UI",
+			"Testing Library",
+			"Vitest",
+		],
+		isDevelop: false,
+	},
+	{
+		title: "AI Image Generation App",
+		code: "https://github.com/victoriacheng15/ai-image-generation#readme",
+		live: "https://ai-image-generation-sandy.vercel.app/",
+		backend: "https://ai-image-generation-ixl2.onrender.com/",
+		description:
+			'Developed a Midjourney/DALL-E clone where users can view the gallery of AI-generated images on the homepage. Users also can create their own AI-generated images by submit their desired prompts or click the "surprise me" button for a random prompt. Lastly, hit the generate button! Like the image? Lets sharing with the community!',
+		techs: [
+			"TypeScript",
+			"React",
+			"React Router",
+			"Node.js",
+			"Express",
+			"MongoDB",
+		],
+		isDevelop: false,
+	},
+];
