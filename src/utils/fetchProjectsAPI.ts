4fdd9085334266ed673bd4236abbe434807baa51
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-25 08:36:21 -0600

    feat: set up fetchPinnedProjects function

---
+++ src/utils/fetchProjectsAPI.ts
@@ -0,0 +1,46 @@
+const GITHUB_API = "https://api.github.com/graphql";
+
+interface Repository {
+	id: string;
+	name: string;
+	description: string;
+	url: string;
+	stargazerCount: number;
+}
+
+export async function getPinnedProjects(): Promise<Repository[]> {
+	const query = `{
+    viewer {
+      pinnedItems(first: 6) {
+        nodes {
+          ... on Repository {
+            id
+            name
+            description
+            url
+            stargazerCount
+          }
+        }
+      }
+    }
+  }`;
+
+	// @ts-expect-error
+	const token: string = import.meta.env.TOKEN;
+
+	const response = await fetch(GITHUB_API, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			authorization: `Bearer ${token}`,
+		},
+		body: JSON.stringify({
+			query: query,
+		}),
+	});
+
+	const json = await response.json();
+	const { nodes } = json.data.viewer.pinnedItems;
+
+	return nodes;
+}
