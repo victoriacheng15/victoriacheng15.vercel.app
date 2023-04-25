3fd7cc6ec11c2658581ec1004fadd4bcf6067a6e
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-25 08:36:37 -0600

    feat: add getPinnedProjects export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { getFormattedDate } from "./date";
 export { elementHasClass, toggleClass } from "./domElement";
 export { sortMDByDate, getUniqueTags, getUniqueTagsWithCount } from "./post";
 export { getReadingTime } from "./readingtime";
+export { getPinnedProjects } from "./fetchProjectsAPI";
