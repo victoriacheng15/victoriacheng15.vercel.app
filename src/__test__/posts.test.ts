df2f18513b50a7193a7f790dad1dedea2e2383b9
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-30 12:34:21 -0600

    chore: correct description for test names

--- src/__test__/posts.test.ts
+++ src/__test__/posts.test.ts
@@ -3,19 +3,19 @@ import { sortMDByDate, getUniqueTags, getUniqueTagsWithCount, getReadingTime } f
 import { posts, expectedPostsByDate, content } from "../__testData__/testData";
 
 describe("sort by date", () => {
-	it("should return", () => {
+	it("should return dat in sorted order", () => {
 		//@ts-expect-error
 		expect(sortMDByDate(posts)).toEqual(expectedPostsByDate);
 	});
 });
 
 describe("unique tags", () => {
-	it("should return tags", () => {
+	it("should return tags without duplications", () => {
 		//@ts-expect-error
 		expect(getUniqueTags(posts)).toEqual(["reflection", "monthly-reflection", "test"]);
 	});
 
-	it("should return tags with count", () => {
+	it("should return unique tags with count", () => {
 		//@ts-expect-error
 		expect(getUniqueTagsWithCount(posts)).toEqual({
 			reflection: 2,
@@ -26,7 +26,7 @@ describe("unique tags", () => {
 });
 
 describe("get reading time", () => {
-	it("should return reading time", () => {
+	it("should return reading time of each blog post", () => {
 		expect(getReadingTime(content)).toEqual(2);
 	});
 });
