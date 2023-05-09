b668fd8cec92c363b7db9a471d815489d0284df0
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-05-09 09:59:46 -0600

    chore: remove unused property for project details (#128)
    * chore: remove unused property for project details
    
    * style: the code has been formatted

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,6 @@ export const projectDetails = [
 	{
 		title: "bubble-tea-api",
 		techs: ["Node.js", "Express", "JavaScript", "MongoDB", "CSS"],
-		isDevelop: false,
 	},
 	{
 		title: "rest-countries-api-next",
@@ -14,16 +13,13 @@ export const projectDetails = [
 			"Testing Library",
 			"Jest",
 		],
-		isDevelop: false,
 	},
 	{
 		title: "hacker-news-next",
 		techs: ["TypeScript", "Next.js", "Redux Toolkit", "Chakra UI", "Testing Library", "Vitest"],
-		isDevelop: false,
 	},
 	{
 		title: "ai-image-generation",
 		techs: ["TypeScript", "React", "React Router", "Node.js", "Express", "MongoDB"],
-		isDevelop: false,
 	},
 ];
