a9d78ce79e432fac82e6ba307e052fee4931c69c
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-05-09 09:49:27 -0600

    Add cypress tests (#127)
    * build: add cypress and its scripts
    
    * build: cypress config
    
    * test: add tests for home, projects, and posts pages

---
+++ cypress/e2e/index.cy.ts
@@ -0,0 +1,25 @@
+describe("homepage", () => {
+	beforeEach(() => {
+		cy.visit("http://localhost:3000/");
+	});
+
+	it("should have the title contain hi there", () => {
+		cy.get("h1").should("contain", "Hi there");
+	});
+
+	it("should have skills title", () => {
+		cy.get("h2").eq(0).should("contain", "Skills");
+	});
+
+	it("should have more than 10 skills on the list", () => {
+		cy.get("ul > .grid").should("have.length.gt", 10);
+	});
+
+	it("should have recent blog title", () => {
+		cy.get("h2").eq(1).should("contain", "Recent blog:");
+	});
+
+	it("should have more than 10 skills on the list", () => {
+		cy.get("ul > .gap-x-2").should("have.length", 5);
+	});
+});
