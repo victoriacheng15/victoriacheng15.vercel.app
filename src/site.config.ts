a17433f5fb7a9daef3174d70f1362204b46ae2f5
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-05-24 10:11:39 -0600

    fix: githubUrl link and update themeColorLight (#161)

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,8 +5,8 @@ const siteMeta = {
 		"As a software developer, my goal is to continuously grow and improve my skills. I am interested in staying current with the latest tech trends and making meaningful contributions to open-source projects.",
 	lang: "en-us",
 	ogLocale: "en-us",
-	githubUrl: "https://github.com/victoriacheng15/personal-blog-site",
-	themeColorLight: "#fafafa",
+	githubUrl: "https://github.com/victoriacheng15/victoriacheng15.vercel.app",
+	themeColorLight: "#f1f5f9",
 	themeColorDark: "#1d1f21",
 };
 
