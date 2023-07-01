7450849fe385d54f20c14b768c491ff4fb724fc7
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-07-01 12:40:05 -0600

    update script devcontainer (#234)
    * chore: remove existed deep work post, change placeholder to txt format
    
    * fix: ensure the array order is based on publish date
    
    * feat: add port forward and commands

--- scripts/publish-post.sh
+++ scripts/publish-post.sh
@@ -10,26 +10,27 @@ files=($(ls -t))
 declare -A publish_dates
 
 for file in "${files[@]}"; do
-  publish_date=$(awk -F'"' '/publishDate:/ {print $2}' "$1" "$file")
-  publish_dates["$file"]=$publish_date
+  if [[ "$file" =~ \.md$ ]]; then
+    publish_date=$(awk -F': ' '/publishDate:/ {print $2}' "$file")
+    publish_dates["$file"]=$publish_date
+  fi
 done
 
 # Sort the publish dates in ascending order using sort
-sorted_files=($(printf "%s\n" "${!publish_dates[@]}" | sort -k2 -t- -r))
+sorted_files=($(printf "%s\n" "${!publish_dates[@]}" | sort -k1 -t-))
 
-echo "List of files sorted by publish date:"
+echo -e "=== List of files sorted by publish date: ===\n"
 for file in "${sorted_files[@]}"; do
   publish_date=${publish_dates["$file"]}
   echo "$publish_date - $file"
 done
-echo "List of files sorted by publish date:"
-
+echo -e "\n=== List of files sorted by publish date: ===\n"
 
 if [[ ${#sorted_files[@]} -gt 0 ]]; then
   first_file=${sorted_files[0]}
 
   echo "Moving $first_file to $markdown_directory"
-  mv "$first_file" "../$markdown_directory"
+  # mv "$first_file" "../$markdown_directory"
   echo "Moved $first_file to $markdown_directory"
 else
   echo "No file found with a publish date."
