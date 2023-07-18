1bdd7c5fb70cd7de76300a625fd28f3e7c3279db
Victoria Cheng <victoria.cheng15@gmail.com>
2023-07-18 10:26:39 -0600

    chore: move testData to __test__ folder

--- src/__test__/posts.test.ts
+++ src/__test__/posts.test.ts
@@ -1,22 +1,19 @@
 import { describe, expect, it } from "vitest";
 import { sortMDByDate, getUniqueTags, getUniqueTagsWithCount, getReadingTime } from "../utils";
-import { posts, expectedPostsByDate, content } from "../__testData__/testData";
+import { posts, expectedPostsByDate, content } from "./testData";
 
 describe("sort by date", () => {
 	it("should return dat in sorted order", () => {
-		//@ts-expect-error
 		expect(sortMDByDate(posts)).toEqual(expectedPostsByDate);
 	});
 });
 
 describe("unique tags", () => {
 	it("should return tags without duplications", () => {
-		//@ts-expect-error
 		expect(getUniqueTags(posts)).toEqual(["reflection", "monthly-reflection", "test"]);
 	});
 
 	it("should return unique tags with count", () => {
-		//@ts-expect-error
 		expect(getUniqueTagsWithCount(posts)).toEqual({
 			reflection: 2,
 			"monthly-reflection": 2,
