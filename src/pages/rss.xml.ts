9d626b158fdb55206a5c1f9cb7bcd7ff3014afde
Victoria Cheng <victoria.cheng15@gmail.com>
2023-09-06 16:02:24 -0600

    chore: update function name to GET and add ts ignore

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,12 +2,13 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import siteMeta from "@/site-config";
 
-export const get = async () => {
+export const GET = async () => {
 	const posts = await getCollection("post");
 
 	return rss({
 		title: siteMeta.title,
 		description: siteMeta.description,
+		// @ts-ignore
 		site: import.meta.env.SITE,
 		items: posts.map((post) => ({
 			title: post.data.title,
