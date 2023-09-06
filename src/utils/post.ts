2855ab1e1c319989e96cfe145e80996339f2cec5
Victoria Cheng <victoria.cheng15@gmail.com>
2023-09-06 16:01:17 -0600

    chore: update functions for posts

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,31 +1,28 @@
 import type { CollectionEntry } from "astro:content";
 
-type Markdown = CollectionEntry<"post">[];
-
-export function sortMDByDate(posts: Markdown = []) {
+export function sortMDByDate(posts: Array<CollectionEntry<"post">>) {
 	return posts.sort(
 		(a, b) => new Date(b.data.publishDate).valueOf() - new Date(a.data.publishDate).valueOf(),
 	);
 }
 
-export function getUniqueTags(posts: Markdown = []) {
-	const uniqueTags = new Set<string>();
-	posts.forEach(({ data: { tags } }) => {
-		tags.map((tag) => uniqueTags.add(tag));
-	});
-	return Array.from(uniqueTags);
+export function getAllTags(posts: Array<CollectionEntry<"post">>) {
+	return posts.flatMap((post) => [...post.data.tags]);
 }
 
-export function getUniqueTagsWithCount(posts: Markdown = []): {
-	[key: string]: number;
-} {
-	return posts.reduce((prev, { data: { tags } }) => {
-		const runningTags: { [key: string]: number } = { ...prev };
-		tags.forEach((tag) => {
-			runningTags[tag] = (runningTags[tag] || 0) + 1;
-		});
-		return runningTags;
-	}, {});
+export function getUniqueTags(posts: Array<CollectionEntry<"post">>) {
+	return [...new Set(getAllTags(posts))];
+}
+
+export function getUniqueTagsWithCount(
+	posts: Array<CollectionEntry<"post">>,
+): Array<[string, number]> {
+	return [
+		...getAllTags(posts).reduce(
+			(acc, t) => acc.set(t, (acc.get(t) || 0) + 1),
+			new Map<string, number>(),
+		),
+	].sort((a, b) => b[1] - a[1]);
 }
 
 const WORDS_PER_MINUTE = 200;
