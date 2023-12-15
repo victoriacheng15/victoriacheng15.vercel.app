9eb3d7ff42260b75eb277de0ebdd47159191e115
Victoria Cheng <victoria.cheng15@Gmail.com>
2023-12-15 08:45:45 -0700

    chore: edit lines based on bash guideline and make echo texts readable in terminal

--- scripts/actions.sh
+++ scripts/actions.sh
@@ -2,7 +2,7 @@
 
 update_main() {
   git switch main
-  git fetch && git pull origin main 
+  git fetch && git pull origin main
 }
 
 actions=(
@@ -14,45 +14,45 @@ actions=(
   "Merge a Pull Request"
 )
 
-PS3="Select the action: " 
+PS3="Select the action: "
 
-select action in "${actions[@]}"
-do
-  case $action in
-    ${actions[0]})
-      update_main
-      break
-      ;;
-    ${actions[1]})
-      scripts/setup-post.sh
-      break
-      ;;
-    ${actions[2]})
-      scripts/publish-post.sh
-      break
-      ;;
-    ${actions[3]})
-      # -f --fill
-      gh pr create -f
-      break
-      ;;
-    ${actions[4]})
-      # -i --interval
-      gh pr checks
-      break
-      ;;
-    ${actions[5]})
-      read -p "Enter the PR number: " pr_number
-      # -s --squash -d --delete-branch
-      if [[ -z $pr_number ]]; then
-        gh pr merge -s -d
-      else
-        gh pr merge -s -d $pr_number
-      fi
-      break
-      ;;
-    *)
-      echo "Invalid option $REPLY"
-      ;;
+select action in "${actions[@]}"; do
+  case "$action" in
+  "${actions[0]}")
+    update_main
+    break
+    ;;
+  "${actions[1]}")
+    scripts/setup-post.sh
+    break
+    ;;
+  "${actions[2]}")
+    scripts/publish-post.sh
+    break
+    ;;
+  "${actions[3]}")
+    # -f --fill
+    gh pr create -f
+    break
+    ;;
+  "${actions[4]}")
+    # -i --interval
+    gh pr checks
+    break
+    ;;
+  "${actions[5]}")
+    read -p "Enter the PR number: " pr_number
+    # -s --squash -d --delete-branch
+    if [[ -z $pr_number ]]; then
+      gh pr merge -s -d
+    else
+      gh pr merge -s -d $pr_number
+    fi
+    break
+    ;;
+  *)
+    echo "Invalid option $REPLY"
+    ;;
   esac
+
 done
