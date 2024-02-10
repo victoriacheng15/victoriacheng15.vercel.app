b373a689ec323ec8bc97b849598fd8642d68d316
Victoria Cheng <victoria.cheng15@gmail.com>
2024-02-09 18:24:08 -0700

    chore: update the script to check the sorted_files

--- scripts/publish-post.sh
+++ scripts/publish-post.sh
@@ -21,21 +21,15 @@ sorted_files=($(for file in "${!publish_dates[@]}"; do
   echo "${publish_dates["$file"]} $file"
 done | sort -k1,1 -t- | awk '{print $2}'))
 
-echo "=== 📅️ List of files sorted by publish date: 📅️ ==="
-for file in "${sorted_files[@]}"; do
-  publish_date=${publish_dates["$file"]}
-  echo "$publish_date - $file"
-done
-
-if [[ ${#sorted_files[@]} -gt 0 ]]; then
+if [[ -n "$sorted_files" ]]; then
   first_file=${sorted_files[0]}
 
   link="${first_file%.*}"
-  echo -e "\n==== 🎦️ The file is moving to the post folder 🎦️ ===\n"
+  echo -e "\n== 🎦️ The file is moving to the post folder 🎦️ ==\n"
   echo "Moving $first_file to $markdown_directory..."
   mv "$first_file" "../$markdown_directory"
-  echo -e "\n==== 🥳️ The file moving is done 🥳️ ===\n"
+  echo -e "\n== 🥳️ The file moving is done 🥳️ ==\n"
   echo "https://victoriacheng15.vercel.app/posts/$link"
 else
-  echo "No file found with a publish date."
+  echo "no files"
 fi
