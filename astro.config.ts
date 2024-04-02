eef632e1c9f6bad6fef11c708d4112bd10e0f201
Victoria Cheng <victoria.cheng15@gmail.com>
2024-04-02 14:18:07 -0600

    chore: add SSR and vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -4,10 +4,13 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://victoriacheng15.vercel.app/",
+	output: "server",
+	adapter: vercel(),
 	markdown: {
 		shikiConfig: {
 			theme: "github-dark",
