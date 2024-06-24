fcaa9dcafbc76c2e6f66f8fded1689109383c93c
Victoria Cheng <victoria.cheng15@gmail.com>
2024-06-24 13:47:40 -0600

    chore: reorder skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,9 +1,5 @@
 export const SKILLS = [
 	{
-		name: "React",
-		skill: "simple-icons:react",
-	},
-	{
 		name: "TypeScript",
 		skill: "simple-icons:typescript",
 	},
@@ -16,36 +12,12 @@ export const SKILLS = [
 		skill: "simple-icons:python",
 	},
 	{
+		name: "React",
+		skill: "simple-icons:react",
+	},
+	{
 		name: "Next.js",
 		skill: "simple-icons:nextdotjs",
-	},
-	{
-		name: "Docker",
-		skill: "simple-icons:docker",
-	},
-	{
-		name: "Jest",
-		skill: "simple-icons:jest",
-	},
-	{
-		name: "Node.js",
-		skill: "simple-icons:nodedotjs",
-	},
-	{
-		name: "Express",
-		skill: "simple-icons:express",
-	},
-	{
-		name: "Redux",
-		skill: "simple-icons:redux",
-	},
-	{
-		name: "Github Actions",
-		skill: "simple-icons:githubactions",
-	},
-	{
-		name: "Testing Library",
-		skill: "simple-icons:testinglibrary",
 	},
 	{
 		name: "Tailwind CSS",
@@ -56,20 +28,44 @@ export const SKILLS = [
 		skill: "simple-icons:sass",
 	},
 	{
-		name: "Cypress",
-		skill: "simple-icons:cypress",
-	},
-	{
-		name: "React Router",
-		skill: "simple-icons:reactrouter",
-	},
-	{
-		name: "Linux",
-		skill: "simple-icons:linux",
+		name: "Redux",
+		skill: "simple-icons:redux",
 	},
 	{
 		name: "Chakra UI",
 		skill: "simple-icons:chakraui",
+	},
+	{
+		name: "Node.js",
+		skill: "simple-icons:nodedotjs",
+	},
+	{
+		name: "Express",
+		skill: "simple-icons:express",
+	},
+	{
+		name: "Jest",
+		skill: "simple-icons:jest",
+	},
+	{
+		name: "Testing Library",
+		skill: "simple-icons:testinglibrary",
+	},
+	{
+		name: "Cypress",
+		skill: "simple-icons:cypress",
+	},
+	{
+		name: "Docker",
+		skill: "simple-icons:docker",
+	},
+	{
+		name: "Github Actions",
+		skill: "simple-icons:githubactions",
+	},
+	{
+		name: "Linux",
+		skill: "simple-icons:linux",
 	},
 	{
 		name: "MongoDB",
